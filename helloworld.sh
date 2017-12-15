9416135bdfe619202345dd8751a2bf2f230dbc3c
zenwarrior <latoyaharrison1981@gmail.com>
2017-12-15 12:38:05 -0500

    Feat Test

--- helloworld.sh
+++ helloworld.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+
 touch helloworld
-printf "hello world/
+printf "hello world
 I wonder what will happen when I do this.
 Sounds fun doesnt it!
 You are so silly." > helloworld
 
+exit 0

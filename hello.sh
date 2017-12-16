d274f6b605b2b12f0e8038214303d2077711215a
wallebee <wltrmbrk@gmail.com>
2017-12-15 19:06:47 -0500

    tempt number5

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#"I thought I put some text in here.
+echo "man can anyone see this?"
+sleep2
+echo "or this"
+sleep5
+echo "OR THIS!!!!!"
+#What the hell man!
+#why can't I see it in git hub"
+#!/ I hope someone can see this!!!

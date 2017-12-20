8c2ea33c43fbc099d73f7ad9e96c698097d9cd2b
wallebee <wltrmbrk@gmail.com>
2017-12-19 22:15:51 -0500

    Here goes nothing again  Please enter the commit message for your changes. Lines starting

---
+++ walters2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# story: a story generator
+#
+# Walter M. Burke
+# CVS: $Header$
+
+shopt -s -o nounset
+
+declare NAME          :# name
+declare COLOR         :# color 
+declare DAY           :# day
+declare RELATIONSHIP  :# a persons relationship
+declare OBJECT        :# an everyday object
+declare -a ACTIVITY   :# a list of everyday activities
+
+# Instructions
+
+
+printf "%s\n" "This is a story generator. I will ask you for some"
+printf "%s\n" "common words. Then I will compose a story."
+printf "\n"
+
+
+# Read the variables
+
+
+read -p "Ezell         :" NAME
+read -p "Red           :" COLOR
+read -p "Tuesday       :" DAY
+read -p "Brother       :" RELATIONSHIP
+read -p "Laptop        :" OBJECT 
+read -p "Laughing      :" ACTIVITY [0]
+read -p "Coding        :" ACTIVITY [1]
+printf "n\"
+read -p "Press return/enter to read your story"
+printf "\n"
+
+
+# Write the story
+
+
+
+printf "%s\n" "$DAY at work, $NAAME realized that he had forgotten to pack"
+printf "%s\n" "a lunch. Ignoring the $SHELL prompt, $NAME decided to head"
+printf "%s\n" "out early and grab lunch from the street vendor."
+printf "%s\n" "As he got outside of the front door of his office,"
+printf "%s\n" "$NAME ran into his $RELATIONSHIP carrying a"
+printf "%s\n" "$COLOR $OBJECT. His $RELATIONSHIP remarked that it had"
+printf "%s\n" "been $RANDOM days since $NAME had called. $NAME"
+printf "%s\n" "thought he would have beenoff surfing the net on his" 
+printf "%s\n" "$OSTYPE computer than running into his $RELATIONSHIP. He"
+printf "%s\n" "offered to take the $OBJECT to get polished." \
+	" HE went ${ACTIVITY[0]}"	
+printf "%s\n" "down the street, wishing that his $RELATIONSHIP had stayed"
+printf "%s\n" "home ${ACTIVITY[1]} anstead."
+
+exit 0
